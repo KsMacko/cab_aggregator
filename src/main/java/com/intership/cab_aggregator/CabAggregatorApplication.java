d200e213cbcd66726a2e433074e01fe0739c45bc
KsMacko <Adeline1397@gmail.com>
2025-03-28 18:05:12 +0300

    init

---
+++ src/main/java/com/intership/cab_aggregator/CabAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.intership.cab_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabAggregatorApplication.class, args);
+	}
+
+}
