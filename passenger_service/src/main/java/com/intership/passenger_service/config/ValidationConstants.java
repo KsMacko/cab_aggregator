20561145ce63b657bc02c1a566a5736cec8cd7d1
KsMacko <Adeline1397@gmail.com>
2025-04-05 16:42:55 +0300

    updated validation constants

--- passenger_service/src/main/java/com/intership/passenger_service/config/ValidationConstants.java
+++ passenger_service/src/main/java/com/intership/passenger_service/config/ValidationConstants.java
@@ -6,6 +6,8 @@ public interface ValidationConstants {
     String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
     String ALPHANUMERIC_REGEX = "^[a-zA-Z0-9]+$";
 
-
+    int MAX_NAME_LENGTH = 20;
+    int MAX_PHONE_LENGTH = 12;
+    int MAX_EMAIL_LENGTH = 50;
 
 }
--- passenger_service/src/main/java/com/intership/passenger_service/dto/ProfileDto.java
+++ passenger_service/src/main/java/com/intership/passenger_service/dto/ProfileDto.java
@@ -11,16 +11,16 @@ import jakarta.validation.constraints.Size;
 public record ProfileDto (
         @Positive(message = "passenger.id.invalidInput")
         Long profileId,
-        @Size(max = 20, message = "passenger.name.size")
+        @Size(max = ValidationConstants.MAX_NAME_LENGTH, message = "passenger.name.size")
         @Pattern(regexp = ValidationConstants.CYRILLIC_REGEX, message = "passenger.name.invalidInput")
         @NotNull(message = "passenger.name.notNull")
         String firstName,
         @Schema(example = "example@gmail.com")
         @Pattern(regexp = ValidationConstants.EMAIL_PATTERN, message = "passenger.email.invalidInput")
-        @Size(max = 50)
+        @Size(max = ValidationConstants.MAX_EMAIL_LENGTH)
         String email,
         @Pattern(regexp = ValidationConstants.PHONE_PATTERN, message = "passenger.phone.invalidInput")
-        @Size(max = 12)
+        @Size(max = ValidationConstants.MAX_PHONE_LENGTH)
         @Schema(example = "375336666666")
         String phone,
         @Schema(hidden = true)
